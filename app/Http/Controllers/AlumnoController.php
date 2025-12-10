df0f2470e4fd3c716ebb879bf8bdee49f44d387a
Jarotho <jarothmoo@gmail.com>
2025-12-10 00:24:52 -0600

    cambios

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 
 
